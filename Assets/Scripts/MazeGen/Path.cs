001d9e9f639dd3887f391e08c83927e85415002b
Wutipong Wongsakuldej <mr_tawan@hotmail.com>
2021-12-03 04:09:58 +0700

    add Path. Maze seems to be invalid.

---
+++ Assets/Scripts/MazeGen/Path.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MazeGen
+{
+    public struct PathNode
+    {
+        public int Cell { get; internal set; }
+        public Direction FromDirection { get; internal set; }
+        public Direction ToDirection { get; internal set; }
+    }
+    public class Path
+    {
+        private List<PathNode> nodes = new List<PathNode>();
+
+        public PathNode this[int index] { get { return nodes[index]; } }
+        public int Count { get { return nodes.Count; } }
+
+        private static Direction Next(Direction direction)
+        {
+            return direction switch
+            {
+                Direction.Invalid => Direction.North,
+                Direction.North => Direction.East,
+                Direction.East => Direction.South,
+                Direction.South => Direction.West,
+                Direction.West => Direction.Invalid,
+                _ => Direction.Invalid,
+            };
+        }
+        public static Path DepthFirstSearch(Maze maze, int from, int to)
+        {
+            Path path = new();
+            path.nodes.Add(new PathNode
+            {
+                Cell = from,
+                FromDirection = Direction.Invalid,
+                ToDirection = Direction.Invalid
+            });
+
+            if (from == to) { return path; }
+
+            while(true)
+            {
+                var currentIndex = path.nodes.Count-1;
+                var current = path.nodes[currentIndex];
+                if (current.Cell == to) break;
+
+                var cell = maze[current.Cell];
+                current.ToDirection = Next(current.ToDirection);
+                path.nodes[currentIndex] = current;
+
+                if(current.ToDirection == Direction.Invalid)
+                {
+                    path.nodes.RemoveAt(currentIndex);
+                    continue;
+                }
+
+                if(current.ToDirection == current.FromDirection)
+                {
+                    continue;
+                }
+
+                var nextCell = cell.ConnectedCellID(current.ToDirection);
+                if(nextCell == Maze.InvalidCell)
+                {
+                    continue;
+                }
+
+                var next = new PathNode
+                {
+                    Cell = maze.AdjacentCellID(current.Cell, current.ToDirection),
+                    FromDirection = current.ToDirection.Opposite(),
+                    ToDirection = Direction.Invalid,
+                };
+
+                path.nodes.Add(next);
+            }
+
+            return null;
+        }
+    }
+}
---
+++ Assets/Scripts/Tests/Path.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MazeGen;
+
+public class PathTest
+{
+    [Test]
+    public void TestDepthFirstSearch()
+    {
+        Maze m = MazeGenerator.Generate(6, 5);
+        Path p = Path.DepthFirstSearch(m, 2, 20);
+
+        Assert.IsNotNull(p);
+        Assert.AreEqual(2, p[0]);
+        Assert.AreEqual(20, p[p.Count -1]);
+        
+    }
+}
+
